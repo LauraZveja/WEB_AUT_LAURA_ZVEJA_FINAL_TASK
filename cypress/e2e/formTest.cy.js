f0a2ee7f63a86d044c5ba1fd5fcbf27f070278f6
Laura <lauramaisele@hotmail.lv>
2023-06-07 20:41:06 +0300

    removed .only from second scenario

--- cypress/e2e/formTest.cy.js
+++ cypress/e2e/formTest.cy.js
@@ -12,7 +12,7 @@ describe('Form Test', () => {
         FormPage.validateFormData();
     });
 
-    it.only('fills out the form and validates data with dynamic variables', () => {
+    it('fills out the form and validates data with dynamic variables', () => {
         DynamicFormPage.fillFormWithDynamicData("Laura", "Zveja", "laurazveja@example.com", "Female", "1234567890", "6 April 1991", "Economics", "Reading", "files/random-image.jpg", "Inženieru iela 101A, Ventspils, LV-3601", "NCR", "Delhi");
         DynamicFormPage.submitForm();
         DynamicFormPage.validateFormData("Laura", "Zveja", "laurazveja@example.com", "Female", "1234567890", "06 April,1991", "Economics", "Reading", "random-image.jpg", "Inženieru iela 101A, Ventspils, LV-3601", "NCR", "Delhi");
